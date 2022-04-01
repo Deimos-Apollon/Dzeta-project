b0fc7f4b5391e21a42facebdabdf054905947590
Emelyanov_Dmitriy <dima31120251@gmail.com>
2022-04-01 18:28:02 +0300

    Added docstrings in read_data functions and renamed some variables in them

--- data_reading/data_reading.py
+++ data_reading/data_reading.py
@@ -2,41 +2,76 @@ from flint import *
 import numpy as np
 
 
-def read_from_file(k, m, filename=None):
-    """Read first k nulls with m precision
-    \n k == -1 means reading all nulls"""
-    if not filename:
-        filename = "/media/deimos/Мои_файл_/PyCharmProjects/" \
-                   "project_on_Riemann_dzeta_function/data_files/NImZetaZero40000_1_40000.val"
-    arr = []
-    ctx.dps = m
+def read_zeros(zeros_num: int, precision: int, filename: str) -> np.array:
+    """
+    Read first 'zeros_num' zeta-function zeros with precision 'precision' digits after
+    integer part from file.
+
+    Parameters
+    ----------
+    zeros_num
+        number of zeros, if -1, then read all 40000 zeros
+    precision
+        zeros precision (in digits after int part)
+    filename
+        file must be "NImZetaZero40000_1_40000.val"
+
+    Returns
+    -------
+    np.array(float)
+        An array of real numbers - coefficients of zeros imaginary parts
+
+    """
+
+    zeroes_arr = []
+    ctx.dps = precision
+    new_zeros_num = 40000 if zeros_num == -1 else zeros_num
+
     with open(filename) as file:
-        for i in range(k):
+        for i in range(new_zeros_num):
             number_str = file.readline()
-            # берем срез с нулевого индекса до индекса "." + точность m
-            number_str = number_str[:number_str.find('.') + m + 1]
-            arr.append(number_str)
-    return np.array(arr)
-
-
-def read_zeros_conjugate(k, m, filename=None):
-    """Read first k nulls with m precision
-    #         \n k == -1 means reading all nulls"""
-    if not filename:
-        filename = "/media/deimos/Мои_файл_/PyCharmProjects/" \
-                   "project_on_Riemann_dzeta_function/data_files/NImZetaZero40000_1_40000.val"
-    arr = []
-
-    with open(filename) as file:
-        for i in range(k):
-            number_str = file.readline()
-            # ищем индекс знака .
             int_part_len = number_str.find('.')
-            # берем срез с нулевого индекса до индекса "." + точность m
-            number_str = number_str[:int_part_len + m + 1]
-            ctx.dps = m + int_part_len
+            # slice from 0 index to index of "." (int part) + precision after int part
+            number_str = number_str[:int_part_len + precision + 1]
+            ctx.dps = precision + int_part_len
+            zeroes_arr.append(arb(number_str))
+    return np.array(zeroes_arr)
+
+
+def read_zeros_conjugate(zeros_num: int, precision: int, filename: str) -> np.array:
+    """
+    Read first 'zeros_num' zeta-function zeros with precision 'precision' digits after
+    integer part from file.
+
+    Parameters
+    ----------
+    zeros_num
+        number of zeros, if -1, then read all 40000 zeros
+    precision
+        zeros precision (in digits after int part)
+    filename
+        file must be "NImZetaZero40000_1_40000.val"
+
+    Returns
+    -------
+    np.array(float)
+        An array of complex numbers - zeta-function zeros, conjugate zeros are grouped
+    """
+
+    zeroes_arr = []
+    new_zeros_num = 40000 if zeros_num == -1 else zeros_num
+
+    with open(filename) as file:
+        for i in range(new_zeros_num):
+            number_str = file.readline()
+
+            int_part_len = number_str.find('.')
+            # slice from 0 index to index of "." (int part) + precision after int part
+            number_str = number_str[:int_part_len + precision + 1]
+            ctx.dps = precision + int_part_len
+
             number = acb("1/2", number_str)
-            arr.append(number)
+            zeroes_arr.append(number)
             number = acb("1/2", f"-{number_str}")
-            arr.append(number)
-    return np.array(arr)
+            zeroes_arr.append(number)
+    return np.array(zeroes_arr)
